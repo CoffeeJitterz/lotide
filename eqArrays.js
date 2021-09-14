5efa4824c7bcc4478b0e8345f4fbe8ba965a623a
Josh Millard <josh.millard10@gmail.com>
2021-09-14 18:49:42 -0400

    eqArray.js added

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (input1, input2) {
+//checking if lengths match, if they don't it must be false
+  if (input1.length === input2.length) {
+    for (let i = 0; i < input1.length; i++) {
+      if (input1[i] !== input2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
